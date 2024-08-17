9c2cdefa3f481f936ed5f9583e9d98009390c03e
Renato Macedo <renatolmb@hotmail.com>
2024-08-17 16:09:09 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("jornada de dados")
+
+# Variáveis
+idade = 34
+nome = "Luciano"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertidade = lista.reverse()
